e2dbb4bbe1c46cd61ab7bc3f1a18fd7faca43d65
Alex Padilla <0xpadimaster@gmail.com>
2024-01-23 18:00:27 -0500

    feat: update roadmap data

--- src/lib/roadmap.lib.ts
+++ src/lib/roadmap.lib.ts
@@ -1,12 +1,43 @@
 export const roadmapItems = [
   {
+    title: "Competencia Mundial",
+    subtitle: "Hult Prize",
+    description: [
+      "Cada año, Hult Prize desafía a los jóvenes a resolver los problemas más apremiantes del mundo a través del emprendimiento social innovador.",
+    ],
+    id_item: "page-4",
+    props:
+      "absolute sm:top-[50%] top-[20%] sm:left-[8%] left-[20%] font-bold sm:text-4xl text-xl z-10 pointer-events-none",
+  },
+  {
+    title: "OnCampus Program",
+    subtitle: "Escuela Politécnica Nacional",
+    description: [
+      "El OnCampus Program lleva Hult Prize directamente a las universidades del mundo. Líderes estudiantiles organizarán su Competición Local con el respaldo de la Fundación Hult Prize.",
+    ],
+    id_item: "page-4",
+    props:
+      "absolute sm:top-[50%] top-[20%] sm:left-[8%] left-[20%] font-bold sm:text-4xl text-xl z-10 pointer-events-none",
+  },
+  {
+    title: "Local Competition",
+    subtitle: "Escuela Politécnica Nacional",
+    description: [
+      "Los equipos de la Escuela Politécnica Nacional presentarán sus ideas ante un panel de expertos. El equipo ganador representará a la universidad en los Regional Summits.",
+    ],
+    id_item: "page-4",
+    props:
+      "absolute sm:top-[50%] top-[20%] sm:left-[8%] left-[20%] font-bold sm:text-4xl text-xl z-10 pointer-events-none",
+  },
+  {
     title: "Step 1: Qualifiers",
     subtitle: "November - February",
     description: [
       "El OnCampus Program y la Aplicación Abierta son la primera fase de la competición Hult Prize. Los ganadores de los OnCampus Program representarán a sus universidades en las Cumbres Globales. Como alternativa, los equipos pueden presentar su solicitud directamente a través de nuestra Aplicación Abierta.",
     ],
     id_item: "page-5",
-    props: "absolute sm:top-[15%] top-[15%] sm:right-[3%] right-[5%] text-[black]  font-bold z-10 opacity-0 pointer-events-none sm:w-[40%] w-[60%]"
+    props:
+      "absolute sm:top-[15%] top-[15%] sm:right-[3%] right-[5%] text-[black]  font-bold z-10 opacity-0 pointer-events-none sm:w-[40%] w-[60%]",
   },
   {
     title: "Step 2: Regional Summits",
@@ -15,8 +46,8 @@ export const roadmapItems = [
       "En 2024, Hult Prize organizará Cumbres Globales presenciales en ocho ciudades de todo el mundo. Los participantes presentarán sus ideas ante paneles de expertos. Los equipos ganadores de cada cumbre, junto con otros cuatro startups seleccionadas a través de la Ronda de Segunda Oportunidad, obtendrán un lugar en el Global Accelerator.",
     ],
     id_item: "page-7",
-    props: "absolute sm:top-[40%] top-[37%] sm:left-[30%] left-[15%] text-[black] font-bold z-10 opacity-0 pointer-events-none sm:w-[60%] w-[80%]"
-
+    props:
+      "absolute sm:top-[40%] top-[37%] sm:left-[30%] left-[15%] text-[black] font-bold z-10 opacity-0 pointer-events-none sm:w-[60%] w-[80%]",
   },
   {
     title: "Step 3: Global Acelerator",
@@ -26,7 +57,8 @@ export const roadmapItems = [
       "En agosto, viajarán a Londres, Reino Unido, para participar en nuestro Acelerador Global, donde pasarán dos semanas trabajando y aprendiendo con expertos y entrenadores de clase mundial. Al final del Acelerador, se seleccionarán seis equipos para participar en nuestras Finales Globales.",
     ],
     id_item: "page-9",
-    props: "absolute sm:top-[40%] top-[20%] left-[10%] text-[black] font-bold z-10 opacity-0 pointer-events-none w-[70%]"
+    props:
+      "absolute sm:top-[40%] top-[20%] left-[10%] text-[black] font-bold z-10 opacity-0 pointer-events-none w-[70%]",
   },
   {
     title: "Step 4: Global Finals",
@@ -35,6 +67,7 @@ export const roadmapItems = [
       "En las Finales Globales, los equipos presentarán sus emprendimientos sociales ante un panel de distinguidos jueces con la esperanza de ser seleccionados como el próximo ganador del Hult Prize de $1,000,000 USD.",
     ],
     id_item: "page-12",
-    props: "absolute top-[55%] sm:left-[20%] left-[20%] text-[black] font-bold z-10 opacity-0 pointer-events-none w-[70%]"
+    props:
+      "absolute top-[55%] sm:left-[20%] left-[20%] text-[black] font-bold z-10 opacity-0 pointer-events-none w-[70%]",
   },
 ];
