26c1ac938a791b36d2abbd18888645dd34a937b8
Enrique <enrique_piedra2001@hotmail.com>
2024-01-23 17:01:40 -0500

    src: Close #21

--- src/lib/roadmap.lib.ts
+++ src/lib/roadmap.lib.ts
@@ -1,17 +1,12 @@
-export const roadmap = [
-  {
-    title: "The Competition",
-    subtitle: "2024",
-    description: [
-      "Cada año, Hult Prize desafía a los jóvenes a resolver los problemas más apremiantes del mundo a través del emprendimiento social innovador.",
-    ],
-  },
+export const roadmapItems = [
   {
     title: "Step 1: Qualifiers",
     subtitle: "November - February",
     description: [
       "El OnCampus Program y la Aplicación Abierta son la primera fase de la competición Hult Prize. Los ganadores de los OnCampus Program representarán a sus universidades en las Cumbres Globales. Como alternativa, los equipos pueden presentar su solicitud directamente a través de nuestra Aplicación Abierta.",
     ],
+    id_item: "page-5",
+    props: "absolute sm:top-[15%] top-[15%] sm:right-[3%] right-[5%] text-[black]  font-bold z-10 opacity-0 pointer-events-none sm:w-[40%] w-[60%]"
   },
   {
     title: "Step 2: Regional Summits",
@@ -19,6 +14,9 @@ export const roadmap = [
     description: [
       "En 2024, Hult Prize organizará Cumbres Globales presenciales en ocho ciudades de todo el mundo. Los participantes presentarán sus ideas ante paneles de expertos. Los equipos ganadores de cada cumbre, junto con otros cuatro startups seleccionadas a través de la Ronda de Segunda Oportunidad, obtendrán un lugar en el Global Accelerator.",
     ],
+    id_item: "page-7",
+    props: "absolute sm:top-[40%] top-[37%] sm:left-[30%] left-[15%] text-[black] font-bold z-10 opacity-0 pointer-events-none sm:w-[60%] w-[80%]"
+
   },
   {
     title: "Step 3: Global Acelerator",
@@ -27,6 +25,8 @@ export const roadmap = [
       "En julio, los equipos semifinalistas se conectarán con mentores y programas de formación de Hult Prize para perfeccionar sus presentaciones y desarrollar aún más sus negocios.",
       "En agosto, viajarán a Londres, Reino Unido, para participar en nuestro Acelerador Global, donde pasarán dos semanas trabajando y aprendiendo con expertos y entrenadores de clase mundial. Al final del Acelerador, se seleccionarán seis equipos para participar en nuestras Finales Globales.",
     ],
+    id_item: "page-9",
+    props: "absolute sm:top-[40%] top-[20%] left-[10%] text-[black] font-bold z-10 opacity-0 pointer-events-none w-[70%]"
   },
   {
     title: "Step 4: Global Finals",
@@ -34,5 +34,7 @@ export const roadmap = [
     description: [
       "En las Finales Globales, los equipos presentarán sus emprendimientos sociales ante un panel de distinguidos jueces con la esperanza de ser seleccionados como el próximo ganador del Hult Prize de $1,000,000 USD.",
     ],
+    id_item: "page-12",
+    props: "absolute top-[55%] sm:left-[20%] left-[20%] text-[black] font-bold z-10 opacity-0 pointer-events-none w-[70%]"
   },
 ];
