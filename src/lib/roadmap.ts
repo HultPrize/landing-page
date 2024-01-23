2857d9e6cead88befd6bdd69d714b376b07ee34c
Alex Padilla <0xpadimaster@gmail.com>
2024-01-23 11:42:46 -0500

    feat(roadmap): add roadmap data

---
+++ src/lib/roadmap.ts
@@ -0,0 +1,38 @@
+export const roadmap = [
+  {
+    title: "The Competition",
+    subtitle: "2024",
+    description: [
+      "Cada año, Hult Prize desafía a los jóvenes a resolver los problemas más apremiantes del mundo a través del emprendimiento social innovador.",
+    ],
+  },
+  {
+    title: "Step 1: Qualifiers",
+    subtitle: "November - February",
+    description: [
+      "El OnCampus Program y la Aplicación Abierta son la primera fase de la competición Hult Prize. Los ganadores de los OnCampus Program representarán a sus universidades en las Cumbres Globales. Como alternativa, los equipos pueden presentar su solicitud directamente a través de nuestra Aplicación Abierta.",
+    ],
+  },
+  {
+    title: "Step 2: Regional Summits",
+    subtitle: "June",
+    description: [
+      "En 2024, Hult Prize organizará Cumbres Globales presenciales en ocho ciudades de todo el mundo. Los participantes presentarán sus ideas ante paneles de expertos. Los equipos ganadores de cada cumbre, junto con otros cuatro startups seleccionadas a través de la Ronda de Segunda Oportunidad, obtendrán un lugar en el Global Accelerator.",
+    ],
+  },
+  {
+    title: "Step 3: Global Acelerator",
+    subtitle: "July - August",
+    description: [
+      "En julio, los equipos semifinalistas se conectarán con mentores y programas de formación de Hult Prize para perfeccionar sus presentaciones y desarrollar aún más sus negocios.",
+      "En agosto, viajarán a Londres, Reino Unido, para participar en nuestro Acelerador Global, donde pasarán dos semanas trabajando y aprendiendo con expertos y entrenadores de clase mundial. Al final del Acelerador, se seleccionarán seis equipos para participar en nuestras Finales Globales.",
+    ],
+  },
+  {
+    title: "Step 4: Global Finals",
+    subtitle: "September",
+    description: [
+      "En las Finales Globales, los equipos presentarán sus emprendimientos sociales ante un panel de distinguidos jueces con la esperanza de ser seleccionados como el próximo ganador del Hult Prize de $1,000,000 USD.",
+    ],
+  },
+];
