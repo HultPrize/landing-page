615c15f132d672c66fab05ca02db023c0246d560
Johanna Huaraca <86489770+Jahe01@users.noreply.github.com>
2024-10-28 15:04:39 -0500

    Update page.tsx

--- src/app/roadmap/page.tsx
+++ src/app/roadmap/page.tsx
@@ -1,18 +1,46 @@
-import React, { useRef } from "react";
+import React from "react";
+import { Header, Footer } from "@/components/layout/";
 
-import Earth from "@/components/3d-design/Earth";
-import Welcome from "@/components/3d-design/Welcome";
-import Map from "@/components/3d-design/Map";
-
-import ContentMap from "@/components/3d-design/ContentMap";
-
-export default function Home() {
+const Home = () => {
   return (
     <>
-      <div className="w-full h-[100vh] ">
-        <Map />
-        <ContentMap />
+      <div className="bg-white h-full flex flex-col items-center pt-20">
+        {/* La Competencia */}
+        <div className="max-w-4xl mx-auto px-4 py-8">
+          <div className="text-center mb-8">
+            <h2 className="text-4xl font-bold text-[#E5077F]">La Competencia</h2>
+            <h3 className="text-2xl font-bold text-[#E5077F]">Hult Prize 2025</h3>
+          </div>
+
+          <div className="flex flex-col gap-8">
+            {/* Fase Uno */}
+            <div className="flex flex-col items-start transition-transform duration-500 ease-in-out transform hover:scale-105 mb-12 p-4 md:flex-row">
+              <div className="md:w-1/2">
+                <h3 className="text-lg font-bold text-[#E5077F]">Fase Uno:</h3>
+                <p>Hay un ganador en base a un video de 4 minutos y se seleccionan los equipos.</p>
+              </div>
+            </div>
+
+            {/* Fase Semifinal */}
+            <div className="flex flex-col items-start transition-transform duration-500 ease-in-out transform hover:scale-105 mb-12 p-4 md:flex-row-reverse">
+              <div className="md:w-1/2">
+                <h3 className="text-lg font-bold text-[#E5077F]">Fase Semifinal:</h3>
+                <p>En el Teatro Politécnico se va a escoger los equipos.</p>
+              </div>
+            </div>
+
+            {/* Fase Final */}
+            <div className="flex flex-col items-start transition-transform duration-500 ease-in-out transform hover:scale-105 mb-12 p-4 md:flex-row">
+              <div className="md:w-1/2">
+                <h3 className="text-lg font-bold text-[#E5077F]">Fase Final:</h3>
+                <p>Competencia nacional, regional summit ganadores de cada ciudad.</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
-}
+};
+
+export default Home;
