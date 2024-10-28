8368ef6609fd4c88560a2cb55a18721941d33c5c
Johanna Huaraca <86489770+Jahe01@users.noreply.github.com>
2024-10-28 15:09:05 -0500

    Update page.tsx

--- src/app/winners/page.tsx
+++ src/app/winners/page.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-
 import Image from 'next/image';
 
-const winners = () => {
+const Winners = () => {
   return (
-    <div className="bg-slate-50 h-full flex flex-col items-center pt-20">
+    <div className="bg-white h-full flex flex-col items-center pt-20">
 
-      <div className="max-w-4xl mx-auto px-4 py-8 flex flex-col md:flex-row items-center  mb-8">
+      <div className="max-w-4xl mx-auto px-4 py-8 flex flex-col md:flex-row items-center mb-8 transition-transform duration-500 ease-in-out transform hover:scale-105">
         <div className="md:w-1/2 md:pr-8">
           <h2 className="text-4xl font-bold mb-4">Gelwear</h2>
           <p className="mb-6 text-lg">
@@ -15,7 +14,7 @@ const winners = () => {
         </div>
         <div className="md:w-1/2">
           <Image
-            className="w-full h-auto "
+            className="w-full h-auto"
             height={300}
             width={300}
             src="https://hult-prize.s3.amazonaws.com/winners/gel-wear.jpeg"
@@ -23,10 +22,11 @@ const winners = () => {
           />
         </div>
       </div>
-      <div className="max-w-4xl mx-auto px-4 py-8 flex flex-col md:flex-row items-center">
+
+      <div className="max-w-4xl mx-auto px-4 py-8 flex flex-col md:flex-row items-center transition-transform duration-500 ease-in-out transform hover:scale-105">
         <div className="md:w-1/2">
           <Image
-            className="w-full h-auto "
+            className="w-full h-auto"
             height={300}
             width={300}
             src="https://hult-prize.s3.amazonaws.com/winners/innomaps.jpg"
@@ -40,48 +40,53 @@ const winners = () => {
           </p>
         </div>
       </div>
-      {/* <h1 className="text-4xl font-bold mb-4 mt-10">Ganadores Mundiales </h1> */}
-      {/* <div className="max-w-4xl mx-auto px-4 py-8 grid grid-cols-1 md:grid-cols-3 gap-8 w-full"> */}
-        {/* <div className="flex flex-col items-center">
+
+      {/* Ganadores Mundiales */}
+      {/* <h1 className="text-4xl font-bold mb-4 mt-10">Ganadores Mundiales</h1>
+      <div className="max-w-4xl mx-auto px-4 py-8 grid grid-cols-1 md:grid-cols-3 gap-8 w-full">
+        
+        {/* Ganador 1 
+        <div className="flex flex-col items-center transition-transform duration-500 ease-in-out transform hover:scale-105">
           <Image
             className="w-full h-auto object-cover min-h-40 max-h-72 mb-4"
             height={300}
             width={300}
             src="https://proyectoib.s3.us-west-2.amazonaws.com/home/2023-hult-prize-winners-with-stella-mccartney.jpg"
-            alt="Image 1"
+            alt="Banofi Leather"
           />
           <h2 className="text-2xl font-bold mb-2 text-center">Banofi Leather - Ganadores 2023</h2>
           <p className="text-lg mb-4 text-justify">Banofi Leather convierte desechos de cultivo de plátanos en cuero vegano, sostenible y libre de crueldad, reduciendo el impacto ambiental hasta un 95% en agua, 90% en carbono y eliminando residuos tóxicos. Nuestros materiales certificados por PETA y estándares no tóxicos de la industria ya son elegidos por más de 30 marcas, habiendo vendido 400+ cuadernos como prueba de calidad y durabilidad, posicionándonos para expandir esta tecnología sostenible en la moda.</p>
-        </div> */}
+        </div>
 
-        {/* <div className="flex flex-col items-center">
+        {/* Ganador 2 
+        <div className="flex flex-col items-center transition-transform duration-500 ease-in-out transform hover:scale-105">
           <Image
             className="w-full h-auto object-cover min-h-40 max-h-72 mb-4"
             height={300}
             width={300}
             src="https://proyectoib.s3.us-west-2.amazonaws.com/home/eco-bana_hultprize.jpg"
-            alt="Image 2"
+            alt="EcoBana"
           />
           <h2 className="text-2xl font-bold mb-2 text-center">EcoBana - Ganadores 2022</h2>
           <p className="text-lg mb-4 text-justify">EcoBana, ganador del Hult Prize 2022 de la Universidad de St. Paul en Limuru, Kenia, es una empresa social que busca emplear a personas y erradicar la fabricación de plástico en toallas sanitarias, produciendo protectores biodegradables para combatir la pobreza menstrual mediante fibras de plátano y promoviendo una economía verde y circular.</p>
-        </div> */}
+        </div>
 
-        {/* Tercera columna */}
-        {/* <div className="flex flex-col items-center">
+        {/* Ganador 3 
+        <div className="flex flex-col items-center transition-transform duration-500 ease-in-out transform hover:scale-105">
           <Image
             className="w-full h-auto object-cover min-h-40 max-h-72 mb-4"
             src="https://proyectoib.s3.us-west-2.amazonaws.com/home/ecospire.png"
-            alt="Image 3"
+            alt="Ecospire"
             height={300}
             width={300}
           />
           <h2 className="text-2xl font-bold mb-2">Ecospire - Ganadores 2021</h2>
           <p className="text-lg mb-4">Utilizar residuos de alimentos para ofrecerle productos de papel económicos y ecológicos sin talar un solo árbol.</p>
-        </div> */}
-      {/* </div> */}
-      
+        </div>
+      </div> */}
+
     </div>
   );
 };
 
-export default winners;
+export default Winners;
