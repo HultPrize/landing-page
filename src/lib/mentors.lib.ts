eb5dd119e8aa17d0d8bc0f92e8fb98e86620a76c
Cristopher Herrera <82126489+cristoxdxd@users.noreply.github.com>
2024-12-01 09:29:41 +0000

    feat: add lib for mentors

---
+++ src/lib/mentors.lib.ts
@@ -0,0 +1,78 @@
+export interface Mentor {
+    name: string;
+    imageSrc: string;
+    description: string;
+}
+
+export const mentors: Mentor[] = [
+  {
+    name: "Alexandra Maldonado",
+    imageSrc:
+      "https://hult-prize.s3.us-east-1.amazonaws.com/mentors/Alexandra_Maldonado.jpg",
+    description:
+      "Abogada y Maestrante en Compliance y Datos Personales, con múltiples estudios de posgrado en Derecho Digital, Cibercrimen y Derecho Público. Es docente en la Universidad de las Américas y docente investigadora en Ciberderecho. Se desempeña como abogada en libre ejercicio y consultora especializada en datos personales y ciberseguridad.",
+  },
+  {
+    name: "Ana María Loopa",
+    imageSrc:
+      "https://hult-prize.s3.us-east-1.amazonaws.com/mentors/Maria_Loopa.jpg",
+    description:
+      "Fundadora de Loopa, docente de maestría en Ciencia de Datos e Inteligencia Artificial en varias universidades del pais. Apasionada emprendedora, directora de tecnología y mentora, comprometida con impulsar el ecosistema emprendedor.",
+  },
+  {
+    name: "Berioska Torres",
+    imageSrc:
+      "https://hult-prize.s3.us-east-1.amazonaws.com/mentors/Berioska_Torres.jpg",
+    description:
+      "Experta en transformación digital con más de 17 años de experiencia en estrategia empresarial y desarrollo tecnológico. Ha ocupado cargos clave en la promoción de la economía digital en Iberoamérica, como Subsecretaria de la Sociedad de la Información y Directora de ALETI. Actualmente, es Consultora Independiente y Directora de Proyectos en el Clúster de Transformación Digital de Guayaquil. Su enfoque está en impulsar la innovación y competitividad mediante la adopción de nuevas tecnologías.",
+  },
+  {
+    name: "Evely Iza",
+    imageSrc:
+      "https://hult-prize.s3.us-east-1.amazonaws.com/mentors/Evely_Iza.jpg",
+    description:
+      "Gerente de Proyectos y Co-Fundadora de ARCON Constructores, enfocada en innovación y sostenibilidad en comunidades. Project Manager en la Aceleradora de Negocios con IA de Strategia Humana, apoyando a emprendedores en propuestas innovadoras. Magíster en Recursos Hídricos y certificada en Gestión del Cambio y Coaching. Apasionada por promover el empoderamiento femenino y el impacto social positivo.",
+  },
+  {
+    name: "Daniela Medina",
+    imageSrc:
+      "https://hult-prize.s3.us-east-1.amazonaws.com/mentors/Daniela_Medina-1.jpeg",
+    description:
+      "Ingeniera Agrónoma y especialista en ciencia de alimentos, docencia universitaria y dirección financiera. Actualmente trabaja en Banco Pichincha, desarrollando productos financieros para la inclusión de agricultores, y es docente de postgrado en agronegocios. Apasionada por las causas sociales, busca motivar a las mujeres y niñas a perseguir sus sueños, especialmente en ciencias. Su proyecto actual se centra en visibilizar a la mujer agrícola en el mundo financiero y promover la inclusión y el financiamiento para el medioambiente.",
+  },
+  {
+    name: "Nathaly Vinueza",
+    imageSrc:
+      "https://hult-prize.s3.us-east-1.amazonaws.com/mentors/Nathaly_Vinueza.jpg",
+    description:
+      "Abogada y emprendedora con más de 15 años de experiencia en los sectores público y privado. Como Speaker Internacional, se ha destacado por promover la inclusión financiera y la equidad de género. Fundadora de iniciativas como Mujer Segura y WARMI, Nathaly lidera esfuerzos para empoderar a la mujer y reducir la brecha de género en América Latina, convirtiéndose en una figura clave en la transformación del bienestar femenino.",
+  },
+  {
+    name: "Ricardo Gonzalez",
+    imageSrc:
+      "https://hult-prize.s3.us-east-1.amazonaws.com/mentors/RICARDO_GONZALEZ.jpg",
+    description:
+      "Emprendedor y consultor internacional con experiencia en innovación y emprendimiento. MBA por la Universidad EAFIT, ha cofundado más de siete emprendimientos y liderado proyectos en neuromarketing y sostenibilidad. Fue Guest Expert en el Hult Prize 2019, asesorando a finalistas internacionales. Actualmente, es CEO de LATINNOVA y Neuromind, y combina su labor empresarial con la docencia en posgrados y la consultoría para organizaciones como la Cámara de Comercio de Quito y CONQUITO.",
+  },
+  {
+    name: "Esteban Espinosa",
+    imageSrc:
+      "https://hult-prize.s3.us-east-1.amazonaws.com/mentors/Esteban_Espinosa.jpg",
+    description:
+      "Ingeniero en Sistemas y Computación, con más de 15 años de experiencia en desarrollo de software y liderazgo de equipos de desarrollo. Ha trabajado en empresas de tecnología en Ecuador y Estados Unidos, y actualmente es Director de Ingeniería en la empresa de tecnología de la información y servicios, Globant. Apasionado por la tecnología y la innovación, busca impulsar el talento local y apoyar a emprendedores en su camino al éxito.",
+  },
+  {
+    name: "Vanesa Gallardo",
+    imageSrc:
+      "https://hult-prize.s3.us-east-1.amazonaws.com/mentors/Vanesa_Gallardo.jpg",
+    description:
+      "Experta en gestión empresarial y desarrollo humano, Coach Profesional (ACC) y Gerente General de Strategia Humana. Co-Fundadora de Global You Coaching Academy, mentora en WeXchange BID LAB y conferencista internacional. Ha liderado iniciativas como Embajadoras del Cambio y es autora de Mujeres Inspirando Mujeres. Reconocida por el Congreso de EE. UU. y WEConnect International, es consultora para organizaciones como GIZ y CARE.",
+  },
+  {
+    name: "William Villacis",
+    imageSrc:
+      "https://hult-prize.s3.us-east-1.amazonaws.com/mentors/William_Villacis.JPG",
+    description:
+      "Emprendedor serial apasionado por la construcción de proyectos, productos y LEGOS, enfocado en el desarrollo exponencial de personas y organizaciones. Cree que los problemas creativos son oportunidades para innovar. Actualmente, es Gerente de Innovación en La Mina, consultor externo en la Cámara de Comercio de Quito y Kruger Corp., CEO de RDS y Founder de Rapifuego.",
+  },
+];
