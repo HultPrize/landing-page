e35cb6a7b3e5af5165f99fa48f637ade65f06df1
Cristopher Herrera <82126489+cristoxdxd@users.noreply.github.com>
2024-12-04 11:57:45 -0500

    Update page.tsx

--- src/app/winners/page.tsx
+++ src/app/winners/page.tsx
@@ -53,57 +53,11 @@ const Winners = () => {
             className="w-full h-auto"
             height={300}
             width={300}
-            src="https://hult-prize.s3.us-east-1.amazonaws.com/winners/unknown.webp"
+            src="https://hult-prize.s3.us-east-1.amazonaws.com/winners/unknown.png"
             alt="Futuro Emprendimiento"
           />
         </div>
       </div>
-
-
-      {/* Ganadores Mundiales */}
-      {/* <h1 className="text-4xl font-bold mb-4 mt-10">Ganadores Mundiales</h1>
-      <div className="max-w-4xl mx-auto px-4 py-8 grid grid-cols-1 md:grid-cols-3 gap-8 w-full">
-        
-        {/* Ganador 1 
-        <div className="flex flex-col items-center transition-transform duration-500 ease-in-out transform hover:scale-105">
-          <Image
-            className="w-full h-auto object-cover min-h-40 max-h-72 mb-4"
-            height={300}
-            width={300}
-            src="https://proyectoib.s3.us-west-2.amazonaws.com/home/2023-hult-prize-winners-with-stella-mccartney.jpg"
-            alt="Banofi Leather"
-          />
-          <h2 className="text-2xl font-bold mb-2 text-center">Banofi Leather - Ganadores 2023</h2>
-          <p className="text-lg mb-4 text-justify">Banofi Leather convierte desechos de cultivo de plátanos en cuero vegano, sostenible y libre de crueldad, reduciendo el impacto ambiental hasta un 95% en agua, 90% en carbono y eliminando residuos tóxicos. Nuestros materiales certificados por PETA y estándares no tóxicos de la industria ya son elegidos por más de 30 marcas, habiendo vendido 400+ cuadernos como prueba de calidad y durabilidad, posicionándonos para expandir esta tecnología sostenible en la moda.</p>
-        </div>
-
-        {/* Ganador 2 
-        <div className="flex flex-col items-center transition-transform duration-500 ease-in-out transform hover:scale-105">
-          <Image
-            className="w-full h-auto object-cover min-h-40 max-h-72 mb-4"
-            height={300}
-            width={300}
-            src="https://proyectoib.s3.us-west-2.amazonaws.com/home/eco-bana_hultprize.jpg"
-            alt="EcoBana"
-          />
-          <h2 className="text-2xl font-bold mb-2 text-center">EcoBana - Ganadores 2022</h2>
-          <p className="text-lg mb-4 text-justify">EcoBana, ganador del Hult Prize 2022 de la Universidad de St. Paul en Limuru, Kenia, es una empresa social que busca emplear a personas y erradicar la fabricación de plástico en toallas sanitarias, produciendo protectores biodegradables para combatir la pobreza menstrual mediante fibras de plátano y promoviendo una economía verde y circular.</p>
-        </div>
-
-        {/* Ganador 3 
-        <div className="flex flex-col items-center transition-transform duration-500 ease-in-out transform hover:scale-105">
-          <Image
-            className="w-full h-auto object-cover min-h-40 max-h-72 mb-4"
-            src="https://proyectoib.s3.us-west-2.amazonaws.com/home/ecospire.png"
-            alt="Ecospire"
-            height={300}
-            width={300}
-          />
-          <h2 className="text-2xl font-bold mb-2">Ecospire - Ganadores 2021</h2>
-          <p className="text-lg mb-4">Utilizar residuos de alimentos para ofrecerle productos de papel económicos y ecológicos sin talar un solo árbol.</p>
-        </div>
-      </div> */}
-
     </div>
   );
 };
